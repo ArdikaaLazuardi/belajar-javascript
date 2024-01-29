ab26547699ca7a9dab72c3cb575dc5e70a1f599d
Ardika Muhammad Lazuardi <lzd25456@gmail.com>
2024-01-29 13:35:48 +0700

    belajar array

---
+++ array.js
@@ -0,0 +1,8 @@
+// var namaarray = new Array()
+var lemari = new Array("dompet", 500, true);
+console.log(lemari[1]);
+console.log(lemari[2]);
+
+lemari[3] = "ktp";
+lemari.push()
+console.log(lemari);
