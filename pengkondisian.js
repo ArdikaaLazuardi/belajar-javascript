9e6d67579fea3d2c17dcc03a2ecf4b28a94b0b54
Ardika Muhammad Lazuardi <lzd25456@gmail.com>
2024-01-29 14:15:41 +0700

    belajar pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,16 @@
+// var x = prompt("masukkan angka : ")
+// // alert("Its A Pleasure To Meet You " + nama)
+
+// if (x % 2 == 0) {
+//     alert(x + " bilangan genap")
+// }else if (x % 2 == 1){
+//     alert(x + " biangan ganjil")
+// }else{
+//     alert("cik isi nu balek nomorna")
+// }
+
+if(confirm("apakah anda manusia?")) {
+    location = "http://google.com"
+} else {
+    alert ("anda dipenjara")
+}
