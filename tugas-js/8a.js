c0175f88d30dbf9b8fdb3ab4d7f8192a1e7df9c5
Ardika Muhammad Lazuardi <lzd25456@gmail.com>
2024-01-29 14:29:31 +0700

    latihan 8a

---
+++ tugas-js/8a.js
@@ -0,0 +1,19 @@
+var nilai = prompt("Masukkan nilai: ");
+
+nilai = Number(nilai);
+
+// Menghitung grade
+var grade;
+if (nilai < 40) {
+  grade = "E";
+} else if (nilai < 65) {
+  grade = "D";
+} else if (nilai < 75) {
+  grade = "C";
+} else if (nilai < 85) {
+  grade = "B";
+} else {
+  grade = "A";
+}
+
+alert("Grade: " + grade);
